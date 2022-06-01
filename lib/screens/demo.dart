610b28dcef1c17f8f18f6ebd693336f2a904bbad
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2022-06-01 11:32:09 +0600

    1.6.22.1

---
+++ lib/screens/demo.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Demo extends StatefulWidget {
+  const Demo({Key? key}) : super(key: key);
+
+  @override
+  State<Demo> createState() => _DemoState();
+}
+
+class _DemoState extends State<Demo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('demo'),
+      ),
+      body: const Center(child: Text('dome')),
+    );
+  }
+}
