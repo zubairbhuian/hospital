4d32eb2f36a2fd8790cef66a3c817455edc44796
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2022-07-19 21:18:19 +0600

    2323

--- lib/screens/service_detail_screen.dart
+++ lib/screens/service_detail_screen.dart
@@ -56,66 +56,103 @@ class ServiceDetailScreen extends StatelessWidget {
                   ))),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 20),
-            const RegularText(
-                text: "Hospital Emergencies: What You Can Do",
-                fontSize: 24,
-                fontWeight: FontWeight.w700),
-            const SizedBox(height: 20),
-            const RegularText(
-              text:
-                  "Dr. Gumoh Brian jon is a Urology Surgeon in Pegion Medical Collage Hospital and is affiliated with the multiple hospitals in the area leverage other's cross platform infomediaries with out of the box strategic theme areas. Seamless myocardinate goal oriented materials through user friendly niches.",
-              color: AppColor.grayLite,
-            ),
-            const SizedBox(height: 20),
-            const RegularText(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20),
+              const RegularText(
+                  text: "Hospital Emergencies: What You Can Do",
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700),
+              const SizedBox(height: 20),
+              const RegularText(
                 text:
-                    "Enthusiastically cultivate frictionless channels for corporate markets. Progressively promote robust.",
-                color: AppColor.grayLite),
-            const SizedBox(height: 20),
-            Container(
-              width: 200.sw,
-              height: 190,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  image: const DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage(
-                        'images/service_1.png',
-                      ))),
-              child: Stack(alignment: Alignment.center, children: [
-                Positioned(
-                  child: SizedBox(
-                    width: 60,
-                    height: 60,
-                    child: Card(
-                      color: const Color.fromARGB(43, 255, 255, 255),
-                      elevation: 0,
-                      shape: RoundedRectangleBorder(
-                          side:
-                              const BorderSide(width: 1, color: AppColor.amber),
-                          borderRadius: BorderRadius.circular(80)),
-                      child: InkWell(
-                        child: const SizedBox(
-                            child: Center(
-                                child: Icon(
-                          Icons.play_arrow,
-                          color: AppColor.white,
+                    "Dr. Gumoh Brian jon is a Urology Surgeon in Pegion Medical Collage Hospital and is affiliated with the multiple hospitals in the area leverage other's cross platform infomediaries with out of the box strategic theme areas. Seamless myocardinate goal oriented materials through user friendly niches.",
+                color: AppColor.grayLite,
+              ),
+              const SizedBox(height: 20),
+              const RegularText(
+                  text:
+                      "Enthusiastically cultivate frictionless channels for corporate markets. Progressively promote robust.",
+                  color: AppColor.grayLite),
+              const SizedBox(height: 20),
+              Container(
+                width: 200.sw,
+                height: 190,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    image: const DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage(
+                          'images/service_1.png',
                         ))),
-                        onTap: () {},
+                child: Stack(alignment: Alignment.center, children: [
+                  Positioned(
+                    child: SizedBox(
+                      width: 60,
+                      height: 60,
+                      child: Card(
+                        color: const Color.fromARGB(43, 255, 255, 255),
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                            side: const BorderSide(
+                                width: 1, color: AppColor.amber),
+                            borderRadius: BorderRadius.circular(80)),
+                        child: InkWell(
+                          child: const SizedBox(
+                              child: Center(
+                                  child: Icon(
+                            Icons.play_arrow,
+                            color: AppColor.white,
+                          ))),
+                          onTap: () {},
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ]),
-            )
-          ],
+                ]),
+              ),
+              const SizedBox(height: 30),
+              const Align(
+                alignment: Alignment.topLeft,
+                child: RegularText(
+                    text: "Service Included",
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600),
+              ),
+              const RegularText(
+                text:
+                    "Seamless myocardinate goal oriented materials through user friendly niches.",
+                color: AppColor.grayLite,
+              ),
+              const SizedBox(height: 30),
+              const Align(
+                alignment: Alignment.topLeft,
+                child: RegularText(
+                    text: "Some examples",
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600),
+              ),
+              const RegularText(
+                  text:
+                      "Seamless myocardinate goal oriented materials through user friendly niches. Medical Collage Hospital and is affiliated with the multiple. Medical Collage Hospital and is affiliated with the multiple.",
+                  color: AppColor.grayLite),
+              const SizedBox(height: 40),
+              SizedBox(
+                width: 199.sw,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: const RegularText(
+                      text: 'Learn More',
+                    )),
+              ),
+              const SizedBox(height: 50),
+            ],
+          ),
         ),
       ),
     );
