052c9f0869e5ad243fcfcc54f41773c6692073d5
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2023-02-05 18:50:48 +0600

    qweqw

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import firebase_auth
 import firebase_core
 import firebase_database
-import path_provider_macos
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
