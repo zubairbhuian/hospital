8eec473557d1800d7a2ddf51f9cf395f938d22f0
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2023-05-03 00:39:37 +0600

    add-four-bindings

--- lib/pages/application/binding.dart
+++ lib/pages/application/binding.dart
@@ -1,10 +1,16 @@
 import 'package:get/get.dart';
 import 'package:hospital/pages/application/index.dart';
+import 'package:hospital/pages/notification/index.dart';
+import 'package:hospital/pages/service/controller.dart';
+import 'package:hospital/pages/settings/index.dart';
 
 class ApplicationBinding implements Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => ApplicationController());
+    Get.lazyPut(() => ServiceController());
+    Get.lazyPut(() => NotificationController());
+    Get.lazyPut(() => SettingsController());
     // TODO: implement dependencies
   }
 }
--- lib/pages/notification/index.dart
+++ lib/pages/notification/index.dart
@@ -1,5 +1,4 @@
 library intro;
 
 export './controller.dart';
-export './binding.dart';
 export './view.dart';
